a814c3048bf70e1fb51caab68a9319de38f88e8d
marc dahan <marcdahan@gmail.com>
2018-08-14 11:13:36 +0200

    sauvegarde de la première version

---
+++ JsonParser.js
@@ -0,0 +1,58 @@
+var JsonParser = function(Columns, fileName) {
+    this.fileName = fileName;
+    this.table = [];
+    this.csv = null;
+    this.jsonCopy = null;
+    if (Object.prototype.toString.call(Columns) === '[object Array]') {
+        this.table.push(Columns);
+    } else {
+        throw "bad parameter";
+    }
+};
+
+JsonParser.prototype.isJSON = function(json) {
+    var bool = false;
+    if (Object.prototype.toString.call(json) === '[object Object]') {
+        bool = true;
+    }
+    return bool;
+};
+
+JsonParser.prototype.jsonToTable = function(json, path) {
+    if (!this.isJSON(json)) {
+        throw "bad parameter" + JSON.parse(json);
+    }
+    $.each(json, $.proxy(function(key, value) {
+        if ($.type(value) == "object") {
+            path = (path ? path + '.' : '');
+            this.jsonToTable(value, path + key);
+        } else if ($.type(value) == "string") {
+            this.table.push([path, key, "" + value]);
+        }
+    }, this));
+};
+
+JsonParser.prototype.tabletoCsv = function() {
+    var pretreated = [];
+    $.each(this.table,function(index, value) {
+        pretreated.push(value.join('|'));
+    });
+    this.csv = pretreated.join('\r');
+};
+
+JsonParser.prototype.jsonToCsv = function(json) {
+    this.jsonCopy = $.extend({}, json);
+    this.jsonToTable(json);
+    this.tabletoCsv(this.table);
+};
+
+
+JsonParser.prototype.exportJsonToCsv = function(json) {
+    this.jsonToCsv(json);
+    var a         = document.createElement('a');
+    a.href        = 'data:text/csv;charset=UTF-8' + escape(this.csv);
+    a.target      = '_blank';
+    a.download    = 'export.csv';
+    document.body.appendChild(a);
+    a.click();
+};
