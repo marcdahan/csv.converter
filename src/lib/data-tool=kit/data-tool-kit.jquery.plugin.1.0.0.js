d5fbb1b6023b175a9eab55e67e571202d1449598
marc dahan <marcdahan@gmail.com>
2018-08-20 07:09:04 +0200

    restructuration

--- src/lib/data-tool=kit/data-tool-kit.jquery.plugin.1.0.0.js
+++ src/lib/data-tool=kit/data-tool-kit.jquery.plugin.1.0.0.js
@@ -1,4 +1,4 @@
-/* jshint -W098 */
+/* jshint -W098, -W002*/
 /*!
  * jQuery-data-tool-kit JavaScript Library v1.0.0
  *
@@ -11,35 +11,42 @@
             var countColumns = 1;
             var $that = $(this);
 			$that.find('.em-csv-add-column').on('click', function(e) {
+                e.stopPropagation();
                 countColumns++;
                 var html = '';
                 html += '<div class="em-column-' + countColumns + '">';
-                html +=     '<label class="em-field em-column-' + countColumns + '-id">';
-                html +=         '<input  type="text" data-column-number="' + countColumns + '" data-column-attribute="title" name="em-column-' + countColumns + '-id" placeholder="id of the column n°' + countColumns + '" autocomplete="on">';
-                html +=     '</label>';
                 html +=     '<label class="em-field em-column-' + countColumns + '-title">';
-                html +=         '<input type="text" data-column-number="' + countColumns + '" data-column-attribute="title" name="em-column-' + countColumns + '-title" placeholder="Title of the column n°' + countColumns + '" autocomplete="on">';
+                html +=         '<input type="text" data-column-number="' + countColumns + '" name="em-column-' + countColumns + '-title" placeholder="Title of the column n°' + countColumns + '" autocomplete="on">';
                 html +=         '<span class="em-explanation"></span>';
                 html +=     '</label>';
                 html += '</div>';
                 $that.find('.em-headers-builder .em-repository').append(html);
 			});
             $that.find('.em-csv-remove-column').on('click', function(e) {
+                e.stopPropagation();
                 if (countColumns > 1) {
                     $that.find('.em-column-' + countColumns).remove();
                     countColumns--;
                 }
 			});
 			$that.on('submit', function(e) {
+                e.stopPropagation();
                 e.preventDefault();
-				var fileName = $that.find("[name*='filename']").val() || 'export';
-                var headers = {};
+                var fileName = $that.find("[name*='filename']").val() || 'export';
+                var headers = [];
 				var $headers = $that.find("[name*='column']");
+                var columnNumber = null;
+                var title = '';
                 $headers.each(function(index, element) {
-
-
+                    columnNumber = $(element).attr('data-column-number');
+                    title = $(element).val();
+                    if (title) {
+                        headers.push(title);
+                    } else {
+                        throw 'Le champs de la colonne n°' + columnNumber + 'est vide.';
+                    }
                 });
-				var json = $that.find("[name*='json']").val() || {
+                var json = {
 					'search': {
 						'No salons found': 'Aucun salon trouvé',
 						"1 salon found": '1 salon trouvé',
@@ -49,6 +56,29 @@
 						'Our Salons in Near Cities': 'Nos salons dans les villes à proximité'
 					}
 				};
+				/*var preJson = $that.find("[name*='json']").val(); || {
+					'search': {
+						'No salons found': 'Aucun salon trouvé',
+						"1 salon found": '1 salon trouvé',
+						'%1d salons found': '%1d salons trouvés',
+						'Franck Provost Hairdressing Salons in near cities': 'Les salons de coiffure Franck Provost dans les villes à proximité',
+						'Franck Provost Hairdressing Salons in near departments': 'Les salons de coiffure Franck Provost dans les départements limitrophes',
+						'Our Salons in Near Cities': 'Nos salons dans les villes à proximité'
+					}
+				};
+                if (!preJson) {
+                    throw 'Le champs Json' + columnNumber + 'est vide.';
+                }
+                try {
+                    json = JSON.parse(preJson.replace(/[\r\n\t]/g, ""));
+                } catch (e) {
+                    throw 'Le champs Json' + columnNumber + 'est invalide.';
+                }
+                if (Object.prototype.toString.call(json) !== '[object Object]') {
+                    throw 'Le champs Json' + columnNumber + 'n\'est pas un objet JSON.';
+                }
+                */
+
 				$.ajax({
 					  method: 'POST',
 					  url: '/dataToolKit/converter/json-to-csv',
